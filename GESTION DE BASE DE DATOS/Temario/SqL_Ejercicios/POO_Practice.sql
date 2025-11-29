a450f786916cb52ffd9f168af58b37820c5d2232
Samuel <samuelperez82pm@gmail.com>
2025-11-29 13:05:36 +0000

    study before project

--- GESTION DE BASE DE DATOS/Temario/SqL_Ejercicios/POO_Practice.sql
+++ GESTION DE BASE DE DATOS/Temario/SqL_Ejercicios/POO_Practice.sql
@@ -70,6 +70,94 @@ begin
   dbms_output.Put_line('Si le sumamos el iva serian ' || p.con_iva);
 end;
 /
+
+
+// Segundo ejercicio inventado
+
+create or replace type t_CostaCoffee as object (
+  id number,
+  employees number,
+  name varchar(50),
   
+  member procedure plantilla,
+  member function mitad_plantilla return number
+  
+);
+/
+
+create or replace type body t_CostaCoffee as
+
+  member procedure plantilla is 
+    begin
+      dbms_output.Put_line('En nuestra plantilla hay : ' || employees || ' personas Pero el empleado del mes a sido: ' || name);
+    end plantilla;
+
+  member function mitad_plantilla return number is
+    begin
+    return employees / 2;
+    end mitad_plantilla;
+end;
+/
+
+declare 
+  Cf t_CostaCoffee  := t_CostaCoffee(1,12,'Ericky');
+    begin
+      Cf.plantilla;
+      dbms_output.put_line('Si calculamos la mitad de la plantilla serian : ' || Cf.mitad_plantilla);
+    end;
+  /
+  
+
+// PL/Sql avanzado
+
+
+Create or replace type t_empleados as table of varchar(50);
+/
+
+create or replace type t_CostaCoffee as object(
+  id number,
+  name_store varchar2(50),
+  empleados t_empleados,
+  
+  member procedure plantilla,
+  member function contar_empleados return number
+);
+/
+  
+create or replace type body t_CostaCoffee as 
+
+  member procedure plantilla is
+      i number := 1;
+  begin
+    dbms_output.put_line('Plantilla de ' || name_store || ': ' );
+      
+      while i <= empleados.Count Loop
+        dbms_output.put_line('Empleado ' || i || ' ' || empleados(i));
+        i:= i+1 ;
+      end loop;
     
+    /*
+    for i in 1 .. empleados.Count Loop
+      dbms_output.put_line('Empleado ' || i || ' ' || empleados(i));
+    end loop;
+    */
+    
+  end plantilla;
+  
+  member function contar_empleados return number is 
+    begin
+      return empleados.Count;
+    end contar_empleados;
+    
+  end;
+  /
+    
+
+declare 
+  CF t_CostaCoffee := t_CostaCoffee(1,'CityWest',t_empleados('Ericky','Samu','Sinem','Natalia','Danka','Gosia','Gary'));
+  begin
+    Cf.plantilla;
+    dbms_output.put_line('Numero total de empleados : ' || Cf.contar_empleados);
+  end;
+/
     
